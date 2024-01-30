a676d27a0e7e7733ff0831896747136f2510364e
kana <k.salon.du.l@gmail.com>
2024-01-30 15:09:09 +0900

    task2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Blog from "./components/Blog";
 import Header from "./components/Header";
+import Post from "./components/Post";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Blog />} />
+          <Route path="/Post/:id" element={<Post />} />
         </Routes>
       </Router>
     </>
